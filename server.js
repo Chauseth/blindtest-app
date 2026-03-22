5b950f548a29f9613cc656a9c0654718d3773fe7
Chauseth <alexandre.chiquet@hotmail.com>
2026-03-22 23:49:27 +0100

    fix server links

--- server.js
+++ server.js
@@ -186,7 +186,7 @@ app.use((req, res, next) => {
 app.use(express.static(path.join(__dirname, 'build')));
 
 // Fallback pour React Router (SPA)
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
